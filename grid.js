8ba63ca1b939d6394073e9c32f303dc94df6b79b
shree-77 <shreedhoni7@gmail.com>
2023-08-22 17:44:01 +0530

    Working properly

---
+++ grid.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('container');
+
+function makeDivs(num) {
+    for (let i = 0; i < num * num; i++) {
+        let cells = document.createElement('div');
+        cells.classList.add('cell')
+        container.appendChild(cells)
+    }
+}
+makeDivs(16);
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        Sketch(button.value);
+    });
+});
+
+function Sketch(color) {
+    const setcolor = document.querySelectorAll('.cell');
+    setcolor.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = color;
+        });
+    });
+}
+
+
